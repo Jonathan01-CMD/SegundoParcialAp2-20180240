ce6a8684f6409aec74de98994ca2bcae2d8aafe8
Jonathan001-cmd <77310361+Jonathan001-cmd@users.noreply.github.com>
2021-06-30 18:55:35 -0400

    Contexto

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -18,7 +18,6 @@ namespace SegundoParcialAp2_20180240.DAL
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
             modelBuilder.Entity<Clientes>().HasData(new Clientes() { ClienteId = 1, Nombres = "FERRETERIA GAMA" });
             modelBuilder.Entity<Ventas>().HasData(new Ventas() { VentaId = 1, Fecha = new DateTime(2020, 09, 01), ClienteId = 1, Monto = 1000, Balance = 1000 });
             modelBuilder.Entity<Ventas>().HasData(new Ventas() { VentaId = 2, Fecha = new DateTime(2020, 10, 01), ClienteId = 1, Monto = 900, Balance = 800 });
