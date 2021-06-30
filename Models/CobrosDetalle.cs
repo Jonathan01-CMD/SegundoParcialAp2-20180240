6c457e883b0bc0636a9479cbe68544344fd19d19
Jonathan001-cmd <77310361+Jonathan001-cmd@users.noreply.github.com>
2021-06-30 00:58:08 -0400

    Add project files.

---
+++ Models/CobrosDetalle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SegundoParcialAp2_20180240.Models
+{
+    public class CobrosDetalle
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int CobroId { get; set; }
+        public Cobros Cobro { get; set; }
+
+        public int VentaId { get; set; }
+        public Ventas Venta { get; set; }
+
+        public double Cobrado { get; set; }
+    }
+}
